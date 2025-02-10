b26dab68bc9365d60d16b4ddd61d12fb01568bea
nnnnadia <tristaonadia@gmail.com>
2025-02-09 21:16:54 -0300

    Exercise 03

--- src/03-optional-properties.problem.ts
+++ src/03-optional-properties.problem.ts
@@ -1,6 +1,11 @@
 import { expect, it } from "vitest";
 
-export const getName = (params: { first: string; last: string }) => {
+interface NameParams {
+  first: string;
+  last?: string;
+}
+
+export const getName = (params: NameParams) => {
   if (params.last) {
     return `${params.first} ${params.last}`;
   }
